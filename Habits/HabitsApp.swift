ed6c16edd666dded416a9e8405fbb007c3652436
Marcel Jäger <marcel.jaeger.a@outlook.com>
2024-09-25 10:26:26 +0200

    Initial Commit

---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Marcel Jäger on 25.09.24.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
